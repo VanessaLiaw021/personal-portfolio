c943b5d2cb9d57be8707ea849896b76aeaba1386
VanessaLiaw021 <vliaw021@gmail.com>
2022-08-08 21:36:14 -0400

    styled about component, finisht about section and skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,99 @@
-import React from 'react'
+//Import required packages
+import React from 'react';
+import Footer from './Footer';
+import AvatarPicture from '../assets/avatar.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJs, faBootstrap, faReact, faNode, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { DiJqueryLogo, DiMongodb, DiNodejsSmall, DiGit } from 'react-icons/di';
 
+//About Component
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div className="main about">
 
+        {/* About Section */}
+        <div className="about-wrapper">
+          <div className="about-content">
+            <h3>Hi, I'm <span>Vanessa Liaw</span></h3>
+            <p>I'm a <span>Web Developer</span> that wants to focus on <span>Front-End Design/Developer</span> <br></br> role. I am passionate in designing and building a clean website 
+            for users to enjoy. <br></br> Having a elegant website and nice user interface is something I value as a <br></br> web developer and as a user.</p>
+          </div>
+          <img src={AvatarPicture} alt=""/>
+        </div>
+
+        {/* Education Section */}
+        <div className="education">
+          <h2>Education</h2>
+
+
+        </div>
+
+        {/* Skills Section */}
+        <div className="skills">
+          <h2>Skills</h2>
+          <p className="description">These are the skills I learn throughout my journey, but still improving in the future:</p>
+
+          <div className="icon">
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faHtml5 } className="skill-icon" />
+              <p>HTML5</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faCss3Alt } className="skill-icon" />
+              <p>CSS3</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faBootstrap } className="skill-icon" />
+              <p>Bootstrap</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faJs } className="skill-icon" />
+              <p>JavaScript</p>
+            </div>
+            <div className="icon-wrapper">
+              <DiJqueryLogo className="skill-icon" />
+              <p>jQuery</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faReact } className="skill-icon" />
+              <p>React</p>
+            </div>
+          </div>
+          
+          <div className="icon">
+            <div className="icon-wrapper">
+              <DiGit className="skill-icon" />
+              <p>Git</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faSquareGithub } className="skill-icon" />
+              <p>GitHub</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faNode } className="skill-icon" />
+              <p>NodeJS</p>
+            </div>
+            <div className="icon-wrapper">
+              <DiNodejsSmall className="skill-icon" />
+              <p>ExpressJS</p>
+            </div>
+            <div className="icon-wrapper">
+              <FontAwesomeIcon icon={ faDatabase } className="skill-icon" />
+              <p>MySQL</p>
+            </div>
+            <div className="icon-wrapper">
+              <DiMongodb className="skill-icon" />
+              <p>MongoDB</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+//Export About Component
 export default About;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,20 @@
 //Import required packages
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faSquareInstagram, faSquareFacebook, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import styled from 'styled-components';
 
 //Styled compponents for footer
 const FooterWrapper = styled.footer`
   text-align: center;
-  padding: 40px;
+  padding: 30px;
   background-color:  #F8C8DC;
   border-top: 2px solid white;
+  letter-spacing: 2px;
 `;
 
 //Footer Component
 const Footer = () => {
   return (
     <FooterWrapper className="footer">
-       <div>
-        <FontAwesomeIcon icon={ faLinkedin } className="icons" />
-        <FontAwesomeIcon icon={ faSquareGithub } className="icons" />
-        <FontAwesomeIcon icon={ faSquareInstagram } className="icons" />
-        <FontAwesomeIcon icon={ faSquareFacebook } className="icons" />
-      </div>
       <p>@2022 Designed and Develop by <span>Vanessa Liaw</span></p>
     </FooterWrapper>
   )
