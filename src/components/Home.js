a318c92bc0c72e745a63604798aa8d0d15ba074c
VanessaLiaw021 <vliaw021@gmail.com>
2022-08-11 10:46:09 -0400

    added links in home button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ProfilePicture from '../assets/profile.jpg';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 //Home Component 
 const Home = () => {
@@ -11,8 +12,8 @@ const Home = () => {
         <img src={ProfilePicture} alt=""/>
         <h2>Hi, I'm <span>Vanessa Liaw</span></h2>
         <p>I'm a <span>Web Developer</span> that focuses on front-end design</p>
-        <button>Learn About Me</button>
-        <button>Exlpore My Portfolio</button>
+        <button><Link to="/about" className="view">Learn About Me</Link></button>
+        <button><Link to="/portfolio" className="view">Explore My Portfolio</Link></button>
       </div>
       <Footer />
     </>
