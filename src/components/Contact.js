a8bfdede5daa8a2d92008e9b3836e23fa66037b3
VanessaLiaw021 <vliaw021@gmail.com>
2022-08-11 11:15:47 -0400

    added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,22 @@ import Footer from './Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faFacebookSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import styled from 'styled-components';
+
+//Styled component for form 
+const Form = styled.form`
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid white;
+  width: 35%;
+  margin: 70px auto 30px auto;
+  padding: 20px 0;
+  background-color:  #F8C8DC;
+  border-radius: 10px;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
+`;
 
 //Contact Component
 const Contact = () => {
@@ -43,6 +59,24 @@ const Contact = () => {
             <p>Charlotte, NC</p>
           </div>
         </div>
+
+        {/* Contact Form */}
+        <Form>
+          <h2 className="contact-me">Contact Me!</h2>
+          <div className="form-group">
+            <label for="fullName">Full Name</label>
+            <input type="text" placeholder="Full Name" id="fullName" />
+          </div>
+          <div className="form-group">
+            <label for="email">Email</label>
+            <input type="text" placeholder="Email" id="email" />
+          </div>
+          <div className="form-group">
+            <label for="message">Message</label>
+            <textarea></textarea>
+          </div>
+          <button className="send">Send</button>
+        </Form>
       </div>
       <Footer />
     </>
